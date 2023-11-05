15de4e18316fcd41db025540f4e60db154a33e12
schleising <stephen@schleising.net>
2023-11-05 17:13:36 +0000

    Add docs

--- MongoConnector/connector.py
+++ MongoConnector/connector.py
@@ -11,6 +11,43 @@ from . import logger
 
 class MongoConnector:
     def __init__(self, server_url: str | None = None, enable_debug: bool = False) -> None:
+        '''
+        Connect to a mongodb server
+        
+        Parameters
+        ----------
+        
+        server_url : str | None
+            The url of the mongodb server to connect to. If None, the server url will be read from a file.
+            
+        enable_debug : bool
+            Enable debug logging
+        
+        Returns
+        -------
+        
+        None
+        
+        Raises
+        ------
+        
+        None
+        
+        Examples
+        --------
+        
+        >>> from MongoConnector import MongoConnector
+        >>> connector = MongoConnector()
+        >>> connector.connected
+        True
+        >>> connector.get_database_list()
+        ['admin', 'config', 'local']
+        >>> connector.get_collection_list('admin')
+        ['system.version']
+        >>> connector.close()
+        >>> connector.connected
+        False
+        '''
         # Set the logging level to DEBUG if enabled
         if enable_debug:
             logger.setLevel(logging.DEBUG)
@@ -18,7 +55,7 @@ class MongoConnector:
         # Check if server url is provided
         if server_url is None:
             # Get server url
-            self._server_url = self.get_server_url()
+            self._server_url = self._get_server_url()
         else:
             # Set server url
             self._server_url = server_url
@@ -46,7 +83,26 @@ class MongoConnector:
             # Close connection on exit
             atexit.register(self.close)
 
-    def get_server_url(self) -> str:
+    def _get_server_url(self) -> str:
+        '''
+        Read the server url from a file
+        
+        Parameters
+        ----------
+        
+        None
+        
+        Returns
+        -------
+        
+        server_url : str
+            The url of the mongodb server
+            
+        Raises
+        ------
+        
+        None
+        '''
         # Read server from config file
         server_path = Path('./server.txt')
 
@@ -64,13 +120,64 @@ class MongoConnector:
             return 'mongodb://localhost:27017/'
 
     def close(self) -> None:
+        '''
+        Close the connection to the mongodb server
+
+        Parameters
+        ----------
+
+        None
+
+        Returns
+        -------
+
+        None
+
+        Raises
+        ------
+
+        None
+        '''
         # Close connection
         self._client.close()
+
+        # Set connected flag
+        self.connected = False
 
         # Log message
         logger.debug(f'Closed connection to server {self._server_url}')
 
     def get_database(self, database_name: str) -> Database | None:
+        '''
+        Get a database from the mongodb server
+        
+        Parameters
+        ----------
+        
+        database_name : str
+            The name of the database to get
+            
+        Returns
+        -------
+        
+        database : Database | None
+            The database if found, None otherwise
+        
+        Raises
+        ------
+        
+        None
+        
+        Examples
+        --------
+        
+        >>> from MongoConnector import MongoConnector
+        >>> connector = MongoConnector()
+        >>> connector.get_database('admin')
+        Database(MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True), 'admin')
+        >>> connector.get_database('not_a_database')
+        None
+        '''
         # Check the database exists
         if database_name in self._client.list_database_names():
             # Log success message
@@ -86,6 +193,39 @@ class MongoConnector:
             return None
 
     def get_collection(self, database_name: str, collection_name: str) -> Collection | None:
+        '''
+        Get a collection from the mongodb server
+
+        Parameters
+        ----------
+
+        database_name : str
+            The name of the database to get
+
+        collection_name : str
+            The name of the collection to get
+
+        Returns
+        -------
+
+        collection : Collection | None
+            The collection if found, None otherwise
+
+        Raises
+        ------
+
+        None
+
+        Examples
+        --------
+
+        >>> from MongoConnector import MongoConnector
+        >>> connector = MongoConnector()
+        >>> connector.get_collection('admin', 'system.version')
+        Collection(Database(MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True), 'admin'), 'system.version', ...)
+        >>> connector.get_collection('admin', 'not_a_collection')
+        None
+        '''
         # Get database
         database = self.get_database(database_name)
 
@@ -108,10 +248,67 @@ class MongoConnector:
             return None
 
     def get_database_list(self) -> list[str]:
+        '''
+        Get a list of databases from the mongodb server
+        
+        Parameters
+        ----------
+        
+        None
+        
+        Returns
+        -------
+        
+        database_list : list[str]
+            The list of databases
+        
+        Raises
+        ------
+        
+        None
+        
+        Examples
+        --------
+        
+        >>> from MongoConnector import MongoConnector
+        >>> connector = MongoConnector()
+        >>> connector.get_database_list()
+        ['admin', 'config', 'local']
+        '''
         # Return list of databases
         return sorted(self._client.list_database_names())
 
     def get_collection_list(self, database_name: str) -> list[str]:
+        '''
+        Get a list of collections from the mongodb server
+
+        Parameters
+        ----------
+
+        database_name : str
+            The name of the database to get
+        
+        Returns
+        -------
+
+        collection_list : list[str]
+            The list of collections
+
+        Raises
+        ------
+
+        None
+
+        Examples
+        --------
+
+        >>> from MongoConnector import MongoConnector
+        >>> connector = MongoConnector()
+        >>> connector.get_collection_list('admin')
+        ['system.version']
+        >>> connector.get_collection_list('not_a_database')
+        []
+        '''
         # Get database
         database = self.get_database(database_name)
 
